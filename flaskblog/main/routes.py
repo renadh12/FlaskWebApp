1eae63e74fd54b534142b5a809f907009b08869c
renadh12 <renadhc@gmail.com>
2020-07-21 00:58:12 -0400

    Fixed thebloglog layout, edited Home page intro, changed top nav

--- flaskblog/main/routes.py
+++ flaskblog/main/routes.py
@@ -27,7 +27,9 @@ def cov():
 
 @main.route('/tech')
 def tech():
-    return render_template('tech.html', title='Tech')
+    page = request.args.get('page', 1, type=int)  # default page set to 1
+    posts = Post.query.order_by(Post.date_posted.desc()).paginate(page=page, per_page=5)
+    return render_template('tech.html', title='Tech', posts=posts)
 
 @main.route('/fut')
 def fut():
