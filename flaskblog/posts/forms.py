894bafd98ce21362f8c674f669bbd1feaf878258
renadh12 <renadhc@gmail.com>
2020-07-22 02:38:08 -0400

    Added CKEditor to create_post

--- flaskblog/posts/forms.py
+++ flaskblog/posts/forms.py
@@ -1,9 +1,10 @@
 from flask_wtf import FlaskForm
 from wtforms import StringField, SubmitField, TextAreaField
 from wtforms.validators import DataRequired
+from flask_ckeditor import CKEditorField
 
 
 class PostForm(FlaskForm):
     title = StringField('Title', validators=[DataRequired()])
-    content = TextAreaField('Content', validators=[DataRequired()])
+    content = CKEditorField('Content', validators=[DataRequired()])
     submit = SubmitField('Post')
